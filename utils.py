7e37da3091679880b3ead14e6fba8031344e1d94
Tomas Galle <tomas.galle@ugent.be>
2024-05-14 21:42:08 +0200

    :sparkles: Add logger to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 import mne
+import logging
+import colorlog
+
+
+def get_logger():
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter('%(log_color)s[%(asctime)s] - %(levelname)s - %(message)s'))
+
+    logger = colorlog.getLogger("TMS research")
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    
+    return logger
+
+# =================================================
 
 def plot_average_response(eeg_data, tmin = -0.05, tmax = 0.2, ymin = None, ymax = None):
     events, event_dict = mne.events_from_annotations(eeg_data)
@@ -16,3 +31,7 @@ def plot_single_response(eeg_data, channel="Pz", tmin = -0.05, tmax = 0.2):
     event_id = event_dict["Stimulus/S  1"]
     epochs = mne.Epochs(eeg_data, events, event_id=event_id, tmin=tmin, tmax=tmax, baseline=None, preload=True, picks=channel)
     epochs.plot(picks=channel, n_epochs=1, show=True, scalings={'eeg': 50e-6})
+    
+# =================================================
+
+
